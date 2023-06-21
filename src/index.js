4b134cc372bbbbb3a6cf832b88effc02d78ec411
Brenda Aguilar <blenda916@gmail.com>
2023-06-21 06:11:56 -0600

    Producto final

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 import validator from './validator.js';
+const inputCard = document.getElementById("c_number");
+const donation = document.getElementById("donation");
+const cardHolder = document.getElementById("card-holder");
+const cardCvv = document.getElementById("card-cvv");
+const button = document.getElementById("sendCard");
+let doValidation = false;
+function validate(){
+  if(inputCard.value.length > 0 && cardHolder.value.length > 0 && cardCvv.value.length === 3 && donation.value.length > 0){
+    if(inputCard.value.search(/[a-z]/i) === -1 && cardCvv.value.search(/[a-z]/i) === -1){
+      if(validator.isValid(inputCard.value) === true){
+        alert(cardHolder.value+" GRACIAS POR TU DONACION DE "+donation.value+"\n\nREALIZADO CON TARJETA\n\n("+validator.validIssuer(inputCard.value)+") "+validator.maskify(inputCard.value));
+      }
+      else{
+        alert(cardHolder.value + " NO SE HA PODIDO REALIZAR LA DONACIÓN, NUMERO DE TARJETA INVÁLIDO.");
+      }
+    }
+    else{
+      if(inputCard.value.search(/[a-z]/i) !== -1){
+        alert("INGRESA SOLO NUMEROS EN EL CAMPO NUMERO DE TARJETA")
+      }
+      if(cardCvv.value.search(/[a-z]/i) !== -1){
+        alert("INGRESA SOLO NUMEROS EN EL CVV")
+      }
+    }
+  }
+  else{
+    alert("ASEGURATE DE LLENAR POR COMPLETO TODOS LOS CAMPOS DE DATOS");
+  }
+}
+button.addEventListener('click',validate,false);
 
-console.log(validator);
+//console.log(validator); //Codigo predeterminado 
--- src/validator.js
+++ src/validator.js
@@ -2,28 +2,57 @@ const validator = {
   isValid: function(card){
     let digitSum = 0;
     let validCard = false;
-    card_number = card.split('');
+    const card_number = card.split('');
     card_number.reverse();
 
     for(let i = 0;i<card_number.length;i++){
-        card_number[i] = parseInt(card_number[i],10);
-        if(i%2 == 1){
-            card_number[i] *= 2;
-            if(card_number[i]>=10){
-                card_number[i] = ((card_number[i] % 10) + ((card_number[i] - (card_number[i] % 10))/10));
-            }
+      card_number[i] = parseInt(card_number[i],10);
+      if(i%2 === 1){
+        card_number[i] *= 2;
+        if(card_number[i]>=10){
+          card_number[i] = ((card_number[i] % 10) + ((card_number[i] - (card_number[i] % 10))/10));
         }
-        digitSum += card_number[i];
+      }
+      digitSum += card_number[i];
     }
 
-    if((digitSum%10) == 0)
-        validCard = true;
+    if((digitSum%10) === 0)
+      validCard = true;
 
+    console.log(validCard);
     return validCard;
   },
   
   maskify: function(card) {
-    maskedCardNumber = card.slice(0, -4).replace(/./g, "#") + card.slice(-4);
+    return card.slice(0, -4).replace(/./g, "#") + card.slice(-4);
+  },
+
+  validIssuer: function(card){
+    switch(parseInt(card.slice(0,4))){
+      case 1800: case 2131:
+        return "JCB"; break;
+      case 2014: case 2149:
+        return "Diner's Club / enRoute"; break;
+      case 6011:
+        return "Discover"; break;
+    }
+    if(parseInt(card.slice(0,3))>=300 && parseInt(card.slice(0,3)) <= 305)
+      return "Diner's Club / enRoute";
+    switch(parseInt(card.slice(0,2))){
+      case 34: case 37:
+        return "American Express"; break;
+      case 36: case 38:
+        return "Diner's Club / enRoute"; break;
+      case 51: case 52: case 53: case 54: case 55:
+        return "MasterCard"; break;
+    }
+    switch(parseInt(card.slice(0,1))){
+      case 3:
+        return "JCB"; break;
+      case 4:
+        return "Visa"; break;
+    }
+    return "";
   }
 };
 
