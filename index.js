80dab52185a099a341624515b8af4e228002a0e5
Brenda Aguilar <blenda916@gmail.com>
2023-06-22 08:30:31 -0600

    Updates

--- index.js
+++ index.js
@@ -6,21 +6,19 @@ const cardCvv = document.getElementById("card-cvv");
 const button = document.getElementById("sendCard");
 function validate(){
   if(inputCard.value.length > 0 && cardHolder.value.length > 0 && cardCvv.value.length === 3 && donation.value.length > 0){
-    if(inputCard.value.search(/[a-z]/i) === -1 && cardCvv.value.search(/[a-z]/i) === -1){
-      if(validator.isValid(inputCard.value) === true){
-        alert(cardHolder.value+" GRACIAS POR TU DONACION DE "+donation.value+"\n\nREALIZADO CON TARJETA\n\n("+validator.validIssuer(inputCard.value)+") "+validator.maskify(inputCard.value));
-      }
-      else{
+    if(inputCard.value.search(/[a-z]/i) === -1 && cardCvv.value.search(/[a-z]/i) === -1 && donation.value > 0){
+      if(validator.isValid(inputCard.value) === true)
+        alert(cardHolder.value+" GRACIAS POR TU DONACION DE $"+donation.value+"(MXN)\n\nREALIZADO CON TARJETA\n\n("+validator.validIssuer(inputCard.value)+") "+validator.maskify(inputCard.value));
+      else
         alert(cardHolder.value + " NO SE HA PODIDO REALIZAR LA DONACIÓN, NUMERO DE TARJETA INVÁLIDO.");
-      }
     }
     else{
-      if(inputCard.value.search(/[a-z]/i) !== -1){
-        alert("INGRESA SOLO NUMEROS EN EL CAMPO NUMERO DE TARJETA")
-      }
-      if(cardCvv.value.search(/[a-z]/i) !== -1){
-        alert("INGRESA SOLO NUMEROS EN EL CVV")
-      }
+      if(inputCard.value.search(/[a-z]/i) !== -1)
+        alert("INGRESA SOLO NUMEROS EN EL CAMPO NUMERO DE TARJETA");
+      if(cardCvv.value.search(/[a-z]/i) !== -1)
+        alert("INGRESA SOLO NUMEROS EN EL CVV");
+      if(donation.value < 1)
+        alert("INGRESA UNA CANTIDAD VÁLIDA PARA DONAR");
     }
   }
   else{
